0546991b36e4f904f30e4f00a859e660d456f404
xReapex <thomas1.robert1@gmail.com>
2022-01-20 15:57:18 +0100

    Change env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { environment } from './../environments/environment';
+import { TMDB_API_KEY } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +13,7 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
 
   getData(){
-    const url =` https://api.themoviedb.org/3/movie/550?api_key=${environment.TMDB_API_KEY}`
+    const url =` https://api.themoviedb.org/3/movie/550?api_key=${TMDB_API_KEY.TMDB_API_KEY}`
     this.http.get(url).subscribe((res)=>{
       this.data = res
       console.log(this.data)
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  TMDB_API_KEY: 'fe8a03499179c3db4bb693fe5da719ec'
 };
+
+export const TMDB_API_KEY = {
+  TMDB_API_KEY: 'fe8a03499179c3db4bb693fe5da719ec'
+}
