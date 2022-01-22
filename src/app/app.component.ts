57e9823d80679ee3d49776654cc9b60e7bca5f6d
xReapex <tr7296228@gmail.com>
2022-01-22 20:41:15 +0100

    :collision: Création Service de films

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { environment } from './../environments/environment';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'TMDB API w/Angular';
-  public data:any = [this.getData()]
-  constructor(private http: HttpClient) {}
-
-  getData(){
-    const url = `https://api.themoviedb.org/3/movie/550?api_key=${environment.tmdb_api_key}`
-    this.http.get(url).subscribe((res)=>{
-      this.data = res;
-      console.log(this.data);
-      return this.data;
-    })
-  }
 }
