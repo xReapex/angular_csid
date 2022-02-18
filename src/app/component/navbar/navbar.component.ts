01a3e04a7331ccd2ad55655560243890a299408b
AigleSan <naje97.njmm@gmail.com>
2022-02-18 09:26:42 +0100

    dark mode feature added

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @Input()  event:any
+  @Output() Emitter = new EventEmitter();
   constructor() { }
 
+  //const checkbox = document.querySelector("#checkbox");
+  //html = document.getElementsByTagName("html");
+ 
+
   ngOnInit(): void {
-  }
+
+   
 
 }
+
+
+
+toggleDarkMode() {
+
+  console.log("dark mode");
+  
+  let html = document.getElementsByTagName('html')[0]
+    if(html.classList.contains("dark")){
+      html.classList.remove("dark");
+    } else {
+     html.classList.add("dark");
+    }
+   // checkbox.checked
+    //? html.classList.add("dark")      
+    //: html.classList.remove("dark");
+    this.Emitter.emit(event);
+  }
+
+//calling the function directly
+
+  //toggleDarkMode();
+  //checkbox.addEventListener("click",toggleDarkMode);
+  
+  }
+
