2b9c5f20ec908a8f0bc67e08ed468b1957149d99
xReapex <tr7296228@gmail.com>
2022-02-18 16:44:53 +0100

    Fill the searchbar :tada:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,6 @@ import { FilmsService } from './service/films.service';
 })
 
 export class AppComponent {
-  /*
-    public movieName: any;
-    public overview: any;
-    public overralNote: any;
-    public genre: any;*/
 
   public discoveredMovies: any = [];
   public discoveredMoviesMissingData: any = [];
@@ -23,17 +18,6 @@ export class AppComponent {
   constructor(private appService: FilmsService) { }
 
   ngOnInit() {
-    let id: string = "510";
-    /*
-        this.appService.getFilmDataById(id).subscribe(response =>{
-            let prod = JSON.stringify(response);
-            //console.log(prod);
-    
-            this.movieName = response['original_title'];
-            this.overview = response['overview'];
-            this.overralNote = response['vote_average'];
-            this.genre = response['genres']['0']['name'];
-        });*/
 
     this.appService.getDiscoverFilmsData().subscribe(response => {
       let movieAmount = 4;
